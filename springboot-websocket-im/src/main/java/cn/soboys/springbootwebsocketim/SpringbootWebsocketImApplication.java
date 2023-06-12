d0dd18a72e71bd4b1a85e084fa83e17e92c5b294
kenx <17521197955@163.com>
2023-06-13 03:44:17 +0800

    使用断言优雅抛出业务异常

---
+++ springboot-websocket-im/src/main/java/cn/soboys/springbootwebsocketim/SpringbootWebsocketImApplication.java
@@ -0,0 +1,13 @@
+package cn.soboys.springbootwebsocketim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebsocketImApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebsocketImApplication.class, args);
+    }
+
+}
---
+++ springboot-websocket-im/src/test/java/cn/soboys/springbootwebsocketim/SpringbootWebsocketImApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.soboys.springbootwebsocketim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebsocketImApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
